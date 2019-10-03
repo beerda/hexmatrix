0813989b7857b2c99e73ee7487a95ca5e87e87a2
Michal Burda <michal.burda@centrum.cz>
2019-10-03 15:21:49 +0200

    Added like.array()

--- R/like.R
+++ R/like.R
@@ -23,9 +23,6 @@ like <- function(...) {
 #' @rdname like
 #' @export
 like.matrix <- function(m, data=.empty, nrow=.empty, ncol=.empty, byrow=FALSE, dimnames=.empty) {
-  if (identical(data, .empty)) {
-    data <- as.vector(m)
-  }
   if (identical(nrow, .empty)) {
     nrow <- base::nrow(m)
   }
@@ -36,5 +33,27 @@ like.matrix <- function(m, data=.empty, nrow=.empty, ncol=.empty, byrow=FALSE, d
     dimnames <- base::dimnames(m)
     dimnames <- list(.fixDimname(dimnames[[1]], nrow), .fixDimname(dimnames[[2]], ncol))
   }
+  if (identical(data, .empty)) {
+    data <- as.vector(m[seq_len(nrow), seq_len(ncol)])
+  }
   matrix(data=data, nrow=nrow, ncol=ncol, byrow=byrow, dimnames=dimnames)
+}
+
+
+#' @rdname like
+#' @export
+like.array <- function(a, data=.empty, dim=.empty, dimnames=.empty) {
+  if (identical(dim, .empty)) {
+    dim <- base::dim(a)
+  }
+  if (identical(dimnames, .empty)) {
+    dimnames <- base::dimnames(a)
+    dimnames <- lapply(seq_along(dim), function(i) {
+      .fixDimname(dimnames[[i]], dim[i])
+    })
+  }
+  if (identical(data, .empty)) {
+    data <- do.call(`[`, c(list(a), lapply(dim, seq_len)))
+  }
+  array(data=data, dim=dim, dimnames=dimnames)
 }
--- tests/testthat/test-like.R
+++ tests/testthat/test-like.R
@@ -9,6 +9,12 @@ test_that("like matrix", {
   expect_equal(like(m, data=NA, nrow=3),
                matrix(NA, nrow=3, ncol=3, dimnames=list(LETTERS[1:3], letters[1:3])))
 
+  expect_equal(like(m, nrow=3),
+               m[1:3, ])
+
+  expect_equal(like(m, ncol=2),
+               m[, 1:2])
+
 
   m <- matrix(1:12, nrow=4, byrow=FALSE)
   rownames(m) <- LETTERS[1:4]
@@ -18,4 +24,24 @@ test_that("like matrix", {
 
   expect_equal(like(m, data=NA, nrow=3),
                matrix(NA, nrow=3, ncol=3, dimnames=list(LETTERS[1:3], NULL)))
+})
+
+
+test_that("like array", {
+  a <- array(1:24,
+             dim=c(3,4,2),
+             dimnames=list(letters[1:3], LETTERS[1:4], 1:2))
+
+  expect_equal(like(a, data=NA),
+               array(NA,
+                     dim=c(3,4,2),
+                     dimnames=list(letters[1:3], LETTERS[1:4], 1:2)))
+
+  expect_equal(like(a, data=NA, dim=c(2,2,2)),
+               array(NA,
+                     dim=c(2,2,2),
+                     dimnames=list(letters[1:2], LETTERS[1:2], 1:2)))
+
+  expect_equal(like(a, dim=c(2,2,2)),
+               a[1:2, 1:2, 1:2])
 })
