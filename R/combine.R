aa1bd68483ad8a5fbcab33342a0ad12bfb7c10f8
Michal Burda <michal.burda@centrum.cz>
2020-03-10 14:22:06 +0100

    Added documentation

--- R/combine.R
+++ R/combine.R
@@ -1,4 +1,32 @@
+#' Combine lists by elements
+#'
+#' Function returns a list combined by applying a function `f` pairwisely on \eqn{i}-th element
+#' of a list `a` and `b`, for each \eqn{i}. If `a`'s (resp. `b`'s) \eqn{i}-th element is `NULL`,
+#' the function `f` is not called and the result contains the `b`'s (resp. `a`'s) non-`NULL` element
+#' at the \eqn{i}-th position. The resulting list has size of the larger list and the elements of the
+#' shorter input lists are recycled to size of the larger list. The elements are combined by index.
+#' Names of the elements are ignored.
+#'
+#' @param a the first list to be combined
+#' @param b the second list to be combined
+#' @param f the function to be called pairwisely on the elements of `a` and `b` to produce the combinded result
+#' @return an unnamed list of elements
+#'
 #' @export
+#'
+#' @examples
+#' combine(list(1, 2, 3), list(2, 4, 6), f=`+`)
+#' # results in list(3, 6, 9)
+#'
+#' combine(list(1, 2, 3), list(2, 4), f=`+`)
+#' # results in list(3, 6, 3)
+#'
+#' a <- list()
+#' a[[3]] <- 10
+#' b <- list()
+#' a[[5]] <- 20
+#' combine(a, b, f=`+`)
+#' # results in list(NULL, NULL, 10, NULL, 20)
 combine <- function(a, b, f=`+`) {
   assert_that(is.list(a))
   assert_that(is.list(b))
--- R/hexconvol.R
+++ R/hexconvol.R
@@ -1,13 +1,30 @@
-#' Compute convolution over the neighbours.
+#' Compute convolution over neighbours of elements of a hexmatrix.
 #'
-#' @param m a hex-matrix
-#' @param probs a vector of length 7 with probabilities given to the neighbours
-#'     clock-wisely from top-left with "this" coordinate's probability at 7th
-#'     position
+#' The result of this function is a hexmatrix of dimensions equal to `m` with values
+#' computed as a weighted sum of value at the given position and in the neighbourhood
+#' of that position. The weights are given by the `probs` argument.
+#'
+#' @param m a numeric hexmatrix
+#' @param probs a vector of length 7 with six weights corresponding to the neighbours
+#'     clock-wisely from top-left. The 7th value corresponds to the weight of a value
+#'     at the "current" position. Precisely, the weights are understood as follows:
+#'     \enumerate{
+#'        \item `probs[1]` for top-left,
+#'        \item `probs[2]` for top-right,
+#'        \item `probs[3]` for right,
+#'        \item `probs[4]` for bottom-right,
+#'        \item `probs[5]` for bottom-left,
+#'        \item `probs[6]` for left neighbour's weight of the "current" position,
+#'        \item `probs[7]` for the "current" value's weight.
+#'     }
+#' @return a hexmatrix of dimensions equal to `m` with values computed as a convolution
+#'     over the neighbours of `m`'s values
+#'
 #' @export
 hexconvol <- function(m,
                       probs=1) {
   assert_that(is.matrix(m))
+  assert_that(is.numeric(m))
   assert_that(is.vector(probs) & is.numeric(probs))
 
   p <- rep(probs, length.out=7)
