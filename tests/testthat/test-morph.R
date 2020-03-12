c1a89a0b2234557a89c6c1ebbc2a296db6520fcf
Michal Burda <michal.burda@centrum.cz>
2020-03-12 16:07:43 +0100

    Updated morph() test

--- tests/testthat/test-morph.R
+++ tests/testthat/test-morph.R
@@ -6,7 +6,18 @@ test_that("morph (dilation)", {
   expected <- matrix(FALSE, nrow=4, ncol=5)
   expected[c(1, 2, 3, 5, 6, 7, 10, 15, 16, 19, 20)] <- TRUE
   expect_equal(morph(m, dilate=TRUE), expected)
+})
 
+
+test_that("morph (erosion)", {
+  m <- matrix(FALSE, nrow=4, ncol=5)
+  m[c(1, 2, 3, 5, 6, 7, 10, 15, 16, 19, 20)] <- TRUE
+  expected <- matrix(FALSE, nrow=4, ncol=5)
+  expected[c(1, 2, 6, 20)] <- TRUE
+  expect_equal(morph(m, dilate=FALSE), expected)
+
+  m <- matrix(FALSE, nrow=4, ncol=5)
+  m[2, 2] <- TRUE
   expected <- matrix(FALSE, nrow=4, ncol=5)
   expect_equal(morph(m, dilate=FALSE), expected)
 })
