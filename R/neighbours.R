541e5f54702c3b9010eafab39f858b5e3037bf4d
Michal Burda <michal.burda@osu.cz>
2020-10-27 10:59:10 +0100

    Implemented neighbours() for arrays

--- R/neighbours.R
+++ R/neighbours.R
@@ -1,9 +1,15 @@
-#' Returns a list of matrices that represent values of neighbours
+#' Returns an array that represents values of neighbours
 #' in the clock-wise order starting from top-left neighbour
+#' @param m a matrix or array to compute the neighbours from
+#' @param self whether to add the 7-th direction, which is a copy of `m`
+#' @return If `m` is a matrix, the returned value is an array with third
+#'     dimension representing the neighbour direction (1 to 6, or 7 if `self` is
+#'     `TRUE`). If `m` is a 3D array, then then the result is a 4D array with
+#'     the fourth dimension representing the direction.
 #' @export
 #' @import magrittr
 neighbours <- function(m, self=FALSE) {
-  assert_that(is.matrix(m))
+  assert_that(is.hexmatrix(m) || is.hexarray(m))
   assert_that(is.flag(self))
 
   array(c(m %>% shiftDown() %>% shiftRight(FALSE),       # top left neighbours
@@ -13,5 +19,5 @@ neighbours <- function(m, self=FALSE) {
           m %>% shiftUp() %>% shiftRight(FALSE),         # bottom left neightbours
           m %>% shiftRight(TRUE) %>% shiftRight(FALSE),  # left neightbours
           if(self) m else NULL),
-        dim=c(nrow(m), ncol(m), 6 + self))
+        dim=c(dim(m), 6 + self))
 }
--- tests/testthat/test-neighbours.R
+++ tests/testthat/test-neighbours.R
@@ -59,3 +59,39 @@ test_that("neighbours on matrix with odd rows", {
   expect_equal(n[, , 6],
                matrix(c(NA, 1, NA, 3, NA, 5), nrow=3, ncol=2, byrow=TRUE))
 })
+
+test_that("neighbours on array", {
+  m <- matrix(1:12, nrow=4, ncol=3, byrow=TRUE)
+  a <- array(0, dim=c(4, 3, 2))
+  a[, , 1] <- m
+  a[, , 2] <- m
+  n <- neighbours(a)
+
+  expect_true(is.array(n))
+  expect_equal(dim(n), c(4, 3, 2, 6))
+  expect_equal(n[, , 1, 1],
+               matrix(c(NA, NA, NA, NA, 1, 2, 4, 5, 6, NA, 7, 8), nrow=4, ncol=3, byrow=TRUE))
+  expect_equal(n[, , 1, 2],
+               matrix(c(NA, NA, NA, 1, 2, 3, 5, 6, NA, 7, 8, 9), nrow=4, ncol=3, byrow=TRUE))
+  expect_equal(n[, , 1, 3],
+               matrix(c(2, 3, NA, 5, 6, NA, 8, 9, NA, 11, 12, NA), nrow=4, ncol=3, byrow=TRUE))
+  expect_equal(n[, , 1, 4],
+               matrix(c(5, 6, NA, 7, 8, 9, 11, 12, NA, NA, NA, NA), nrow=4, ncol=3, byrow=TRUE))
+  expect_equal(n[, , 1, 5],
+               matrix(c(4, 5, 6, NA, 7, 8, 10, 11, 12, NA, NA, NA), nrow=4, ncol=3, byrow=TRUE))
+  expect_equal(n[, , 1, 6],
+               matrix(c(NA, 1, 2, NA, 4, 5, NA, 7, 8, NA, 10, 11), nrow=4, ncol=3, byrow=TRUE))
+
+  expect_equal(n[, , 2, 1],
+               matrix(c(NA, NA, NA, NA, 1, 2, 4, 5, 6, NA, 7, 8), nrow=4, ncol=3, byrow=TRUE))
+  expect_equal(n[, , 2, 2],
+               matrix(c(NA, NA, NA, 1, 2, 3, 5, 6, NA, 7, 8, 9), nrow=4, ncol=3, byrow=TRUE))
+  expect_equal(n[, , 2, 3],
+               matrix(c(2, 3, NA, 5, 6, NA, 8, 9, NA, 11, 12, NA), nrow=4, ncol=3, byrow=TRUE))
+  expect_equal(n[, , 2, 4],
+               matrix(c(5, 6, NA, 7, 8, 9, 11, 12, NA, NA, NA, NA), nrow=4, ncol=3, byrow=TRUE))
+  expect_equal(n[, , 2, 5],
+               matrix(c(4, 5, 6, NA, 7, 8, 10, 11, 12, NA, NA, NA), nrow=4, ncol=3, byrow=TRUE))
+  expect_equal(n[, , 2, 6],
+               matrix(c(NA, 1, 2, NA, 4, 5, NA, 7, 8, NA, 10, 11), nrow=4, ncol=3, byrow=TRUE))
+})
