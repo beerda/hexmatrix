bb0bb0b8df570ff94f2cb8193c8190703ddfb72b
Michal Burda <michal.burda@osu.cz>
2020-11-02 11:39:14 +0100

    outer2() fixed and documented

--- R/outer2.R
+++ R/outer2.R
@@ -1,4 +1,22 @@
+#' Outer product of multiple arrays
+#'
+#' This function works similarly as `base::outer()` except that it allows the
+#' multiplication of more arguments than just two.
+#'
+#' @param f The function to be used on the outer products
+#' @param ... Vectors, matrices or arrays whose values will be used as arguments
+#'   to the function `f`. The elements of the first argument will be used as
+#'   the first argument to `f`, elements of the second argument will be used as
+#'   the second argument to `f` etc.
+#' @return An array that is an outer product of the input arguments.
 #' @export
+#' @examples
+#' outer2(prod, 1:4)           # returns 1:4
+#' outer2(prod, 1:4, 1:3)      # the same as 1:4 %o% 1:3
+#' outer2(prod, 1:4, 1:3, 1:2) # the same as (1:4 %o% 1:3) %o% 1:2
+#'
+#' # to see how f is called with arguments:
+#' outer2(paste, 1:4, 1:3, 1:2)
 outer2 <- function(f, ...) {
   dots <- lapply(list(...), as.array)
   dims <- lapply(dots, dim)
@@ -17,5 +35,11 @@ outer2 <- function(f, ...) {
     }
     do.call(f, a)
   })
-  array(res, dim=udims)
+
+  res <- array(res, dim=udims)
+  if (length(dim(res)) == 1) {
+    res <- as.vector(res)
+  }
+
+  res
 }
--- tests/testthat/test-outer2.R
+++ tests/testthat/test-outer2.R
@@ -1,4 +1,7 @@
 test_that("outer2", {
+  res <- outer2(prod, 1:4)
+  expect_equal(res, 1:4)
+
   res <- outer2(prod, 1:4, 1:3)
   expect_equal(res, 1:4 %o% 1:3)
 
@@ -6,6 +9,9 @@ test_that("outer2", {
   expect_equal(res, (1:4 %o% 1:3) %o% 1:2)
 
   m <- matrix(1:12, nrow=4)
+  res <- outer2(prod, m)
+  expect_equal(res, m)
+
   res <- outer2(prod, m, 1:3, 1:2)
   expect_equal(res, (m %o% 1:3) %o% 1:2)
 
@@ -14,6 +20,9 @@ test_that("outer2", {
   expect_equal(res, (m %o% m) %o% m)
 
   a <- array(seq_len(2*3*4), dim=c(2,3,4))
+  res <- outer2(prod, a)
+  expect_equal(res, a)
+
   res <- outer2(prod, a, m, 1:3)
   expect_equal(res, (a %o% m) %o% 1:3)
 })
