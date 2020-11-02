4d14b2e7dfe13e498751f98d01de65d3a7bcff43
Michal Burda <michal.burda@osu.cz>
2020-11-02 13:13:19 +0100

    Implemented revertNeighbours() for arrays

--- R/revertNeighbours.R
+++ R/revertNeighbours.R
@@ -1,15 +1,31 @@
 #' @export
 #' @import magrittr
-revertNeighbours <- function (n) {
+revertNeighbours <- function(n) {
   assert_that(is.array(n))
-  assert_that(dim(n)[3] %in% c(6:7))
+  assert_that(length(dim(n)) %in% c(3, 4))
 
-  array(c(n[, , 4] %>% shiftDown() %>% shiftRight(FALSE),       # top left neighbours
-          n[, , 5] %>% shiftDown() %>% shiftLeft(TRUE),         # top right neighbours
-          n[, , 6] %>% shiftLeft(TRUE) %>% shiftLeft(FALSE),    # right neighbours
-          n[, , 1] %>% shiftUp() %>% shiftLeft(TRUE),           # bottom right neighbours
-          n[, , 2] %>% shiftUp() %>% shiftRight(FALSE),         # bottom left neightbours
-          n[, , 3] %>% shiftRight(TRUE) %>% shiftRight(FALSE),  # left neightbours
-          if (dim(n)[3] == 7) n[, , 7] else NULL),              # self
+  if (length(dim(n)) == 3) {
+    # hexmatrix neighbours
+    assert_that(dim(n)[3] %in% c(6:7))
+    f <- function(n, dir) n[, , dir]
+
+  } else if (length(dim(n)) == 4) {
+    # hexarray neighbours
+    assert_that(dim(n)[4] %in% c(6:7))
+    f <- function(n, dir) n[, , , dir]
+  }
+
+  self <- NULL
+  if (dim(n)[length(dim(n))] == 7) {
+    self <- f(n, 7)
+  }
+
+  array(c(f(n, 4) %>% shiftDown() %>% shiftRight(FALSE),       # top left neighbours
+          f(n, 5) %>% shiftDown() %>% shiftLeft(TRUE),         # top right neighbours
+          f(n, 6) %>% shiftLeft(TRUE) %>% shiftLeft(FALSE),    # right neighbours
+          f(n, 1) %>% shiftUp() %>% shiftLeft(TRUE),           # bottom right neighbours
+          f(n, 2) %>% shiftUp() %>% shiftRight(FALSE),         # bottom left neightbours
+          f(n, 3) %>% shiftRight(TRUE) %>% shiftRight(FALSE),  # left neightbours
+          self),
         dim=dim(n))
 }
--- tests/testthat/test-revertNeighbours.R
+++ tests/testthat/test-revertNeighbours.R
@@ -1,4 +1,4 @@
-test_that("revertNeighbours", {
+test_that("revertNeighbours(hexmatrix)", {
   m <- matrix(1:12, nrow=4, byrow=TRUE)
   n <- neighbours(m)
 
@@ -33,11 +33,10 @@ test_that("revertNeighbours", {
                         NA, 5, 6,
                         NA, 8, 9,
                         NA, 11, 12), nrow=4, byrow=TRUE))
-
 })
 
 
-test_that("revertNeighbours with self", {
+test_that("revertNeighbours(hexmatrix) with self", {
   m <- matrix(1:12, nrow=4, byrow=TRUE)
   n <- neighbours(m, TRUE)
 
@@ -73,4 +72,151 @@ test_that("revertNeighbours with self", {
                         NA, 8, 9,
                         NA, 11, 12), nrow=4, byrow=TRUE))
   expect_equal(res[, , 7], m)
+})
+
+
+test_that("revertNeighbours(hexarray)", {
+  m <- matrix(1:12, nrow=4, byrow=TRUE)
+  a <- array(0, dim=c(4, 3, 2))
+  a[, , 1] <- m
+  a[, , 2] <- 12 + m
+  n <- neighbours(a)
+
+  res <- revertNeighbours(n)
+  expect_equal(res[, , 1, 1],
+               matrix(c(NA, NA, NA,
+                        NA, 5, 6,
+                        7, 8, 9,
+                        NA, 11, 12), nrow=4, byrow=TRUE))
+  expect_equal(res[, , 1, 2],
+               matrix(c(NA, NA, NA,
+                        4, 5, 6,
+                        7, 8, NA,
+                        10, 11, 12), nrow=4, byrow=TRUE))
+  expect_equal(res[, , 1, 3],
+               matrix(c(1, 2, NA,
+                        4, 5, NA,
+                        7, 8, NA,
+                        10, 11, NA), nrow=4, byrow=TRUE))
+  expect_equal(res[, , 1, 4],
+               matrix(c(1, 2, NA,
+                        4, 5, 6,
+                        7, 8, NA,
+                        NA, NA, NA), nrow=4, byrow=TRUE))
+  expect_equal(res[, , 1, 5],
+               matrix(c(1, 2, 3,
+                        NA, 5, 6,
+                        7, 8, 9,
+                        NA, NA, NA), nrow=4, byrow=TRUE))
+  expect_equal(res[, , 1, 6],
+               matrix(c(NA, 2, 3,
+                        NA, 5, 6,
+                        NA, 8, 9,
+                        NA, 11, 12), nrow=4, byrow=TRUE))
+
+  expect_equal(res[, , 2, 1],
+               matrix(12 + c(NA, NA, NA,
+                        NA, 5, 6,
+                        7, 8, 9,
+                        NA, 11, 12), nrow=4, byrow=TRUE))
+  expect_equal(res[, , 2, 2],
+               matrix(12 + c(NA, NA, NA,
+                        4, 5, 6,
+                        7, 8, NA,
+                        10, 11, 12), nrow=4, byrow=TRUE))
+  expect_equal(res[, , 2, 3],
+               matrix(12 + c(1, 2, NA,
+                        4, 5, NA,
+                        7, 8, NA,
+                        10, 11, NA), nrow=4, byrow=TRUE))
+  expect_equal(res[, , 2, 4],
+               matrix(12 + c(1, 2, NA,
+                        4, 5, 6,
+                        7, 8, NA,
+                        NA, NA, NA), nrow=4, byrow=TRUE))
+  expect_equal(res[, , 2, 5],
+               matrix(12 + c(1, 2, 3,
+                        NA, 5, 6,
+                        7, 8, 9,
+                        NA, NA, NA), nrow=4, byrow=TRUE))
+  expect_equal(res[, , 2, 6],
+               matrix(12 + c(NA, 2, 3,
+                        NA, 5, 6,
+                        NA, 8, 9,
+                        NA, 11, 12), nrow=4, byrow=TRUE))
+})
+
+
+test_that("revertNeighbours(hexarray) with self", {
+  m <- matrix(1:12, nrow=4, byrow=TRUE)
+  a <- array(0, dim=c(4, 3, 2))
+  a[, , 1] <- m
+  a[, , 2] <- 12 + m
+  n <- neighbours(a, TRUE)
+
+  res <- revertNeighbours(n)
+  expect_equal(res[, , 1, 1],
+               matrix(c(NA, NA, NA,
+                        NA, 5, 6,
+                        7, 8, 9,
+                        NA, 11, 12), nrow=4, byrow=TRUE))
+  expect_equal(res[, , 1, 2],
+               matrix(c(NA, NA, NA,
+                        4, 5, 6,
+                        7, 8, NA,
+                        10, 11, 12), nrow=4, byrow=TRUE))
+  expect_equal(res[, , 1, 3],
+               matrix(c(1, 2, NA,
+                        4, 5, NA,
+                        7, 8, NA,
+                        10, 11, NA), nrow=4, byrow=TRUE))
+  expect_equal(res[, , 1, 4],
+               matrix(c(1, 2, NA,
+                        4, 5, 6,
+                        7, 8, NA,
+                        NA, NA, NA), nrow=4, byrow=TRUE))
+  expect_equal(res[, , 1, 5],
+               matrix(c(1, 2, 3,
+                        NA, 5, 6,
+                        7, 8, 9,
+                        NA, NA, NA), nrow=4, byrow=TRUE))
+  expect_equal(res[, , 1, 6],
+               matrix(c(NA, 2, 3,
+                        NA, 5, 6,
+                        NA, 8, 9,
+                        NA, 11, 12), nrow=4, byrow=TRUE))
+  expect_equal(res[, , 1, 7], m)
+
+  res <- revertNeighbours(n)
+  expect_equal(res[, , 2, 1],
+               matrix(12 + c(NA, NA, NA,
+                        NA, 5, 6,
+                        7, 8, 9,
+                        NA, 11, 12), nrow=4, byrow=TRUE))
+  expect_equal(res[, , 2, 2],
+               matrix(12 + c(NA, NA, NA,
+                        4, 5, 6,
+                        7, 8, NA,
+                        10, 11, 12), nrow=4, byrow=TRUE))
+  expect_equal(res[, , 2, 3],
+               matrix(12 + c(1, 2, NA,
+                        4, 5, NA,
+                        7, 8, NA,
+                        10, 11, NA), nrow=4, byrow=TRUE))
+  expect_equal(res[, , 2, 4],
+               matrix(12 + c(1, 2, NA,
+                        4, 5, 6,
+                        7, 8, NA,
+                        NA, NA, NA), nrow=4, byrow=TRUE))
+  expect_equal(res[, , 2, 5],
+               matrix(12 + c(1, 2, 3,
+                        NA, 5, 6,
+                        7, 8, 9,
+                        NA, NA, NA), nrow=4, byrow=TRUE))
+  expect_equal(res[, , 2, 6],
+               matrix(12 + c(NA, 2, 3,
+                        NA, 5, 6,
+                        NA, 8, 9,
+                        NA, 11, 12), nrow=4, byrow=TRUE))
+  expect_equal(res[, , 2, 7], 12 + m)
 })
