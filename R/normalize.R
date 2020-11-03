27ec3c647d763c2b5ba7d6b2e4fd1615d1be2088
Michal Burda <michal.burda@osu.cz>
2020-11-03 15:53:23 +0100

    Documented normalize()

--- R/normalize.R
+++ R/normalize.R
@@ -1,5 +1,29 @@
+#' Normalize the values of the array by given margin
+#'
+#' By normalization we understand dividing each value of the cell by a sum
+#' computed along the given `MARGIN`. The summation and division may be replaced
+#' with custom functions `SUM` and `DIV`, respectively.
+#'
+#' @param x An array to be normalized
+#' @param MARGIN A margin, over which to perform the normalization.
+#' @param SUM The summationfunction
+#' @param DIV The division function
+#' @return An array of dimensions equal to `x` with normalized values
+#' @examples
+#' m <- matrix(1:12, nrow=4)
+#' r <- normalize(m, 1)   # normalize over the rows
+#' rowSums(r)             # returns a vector of 1s
+#' s <- normalize(m, 2)   # normalize over the columns
+#' colSums(s)             # returns a vector of 1s
 #' @export
 normalize <-function(x, MARGIN=seq_len(length(dim(x) - 1)), SUM=sum, DIV=`/`) {
+  assert_that(is.array(x))
+  assert_that(is.vector(MARGIN))
+  assert_that(is.numeric(MARGIN))
+  assert_that(all(MARGIN %in% seq_along(dim(x))))
+  assert_that(is.function(SUM))
+  assert_that(is.function(DIV))
+
   remain <- setdiff(seq_along(dim(x)), MARGIN)
   perm <- c(MARGIN, remain)
 
