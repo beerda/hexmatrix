d766cbafe4a6b1cbb353f7961af96e22c3b958e9
Michal Burda <michal.burda@osu.cz>
2020-11-03 15:07:42 +0100

    Fixed whichDir() for arrays

--- R/whichDir.R
+++ R/whichDir.R
@@ -1,5 +1,17 @@
+#' Return the number of direction from the current to the other neighbouring
+#' cell in a hexmatrix.
+#'
+#' @param current The index of the actual cell in hexmatrix
+#' @param other The index of the neighbouring cell
+#' @param m The underlying hexmatrix or hexarray
+#' @return A number from 1 to 6 indicating the direction from `current` to
+#'   `other` cell. `NA` is returned if `current` and `other` are not neighbours.
 #' @export
 whichDir <- function(current, other, m) {
+  assert_that(is.number(current))
+  assert_that(is.number(other))
+  assert_that(is.hexmatrix(m) || is.hexarray(m))
+
   res <- .Call('_hexmatrix_whichDir', PACKAGE = 'hexmatrix', current - 1, other - 1, nrow(m), ncol(m))
   ifelse(res < 0, NA, res + 1)
 }
--- src/neighbourIndices.cpp
+++ src/neighbourIndices.cpp
@@ -8,17 +8,20 @@ int yDiffEven[] = {0, 1, 1, 1, 0, -1};
 
 int neigh(int dir, int i, int rows, int cols)
 {
+  int rowcol = rows * cols;
   int curX = i % rows;
   int otherX = curX + xDiff[dir];
   if ((otherX < 0) || (otherX >= rows))
     return -1;
 
-  int curY = i / rows;
+  int curY = (i % rowcol) / rows;
   int otherY = curY + ((curX % 2 == 0) ? yDiffEven[dir] : yDiffOdd[dir]);
   if ((otherY < 0) || (otherY >= cols))
     return -1;
 
-  return otherX + otherY * rows;
+  int otherZ = i / rowcol;
+
+  return otherX + otherY * rows + otherZ * rowcol;
 }
 
 
