a5ae227882a79e13950b9f7dc5301f2e8aa7e047
Michal Burda <michal.burda@osu.cz>
2020-11-08 16:20:18 +0100

    Added as.hexmatrix(), as.hexarray()

--- R/hexmatrix.R
+++ R/hexmatrix.R
@@ -11,6 +11,34 @@ is.hexarray <- function(x) {
 
 
 #' @export
+as.hexmatrix <- function(x, ...) {
+  if (is.hexmatrix(x)) {
+    return(x)
+  } else if (is.hexarray(x)) {
+    if (nlayer(x) > 1) {
+      warning(paste('Conversion of hexarray to hexmatrix:',
+                     (nlayer(x) - 1), 'omitted.'))
+    }
+    return(x[, , 1])
+  } else {
+    stop('Conversion to hexmatrix failed.')
+  }
+}
+
+
+#' @export
+as.hexarray <- function(x, ...) {
+  if (is.hexmatrix(x)) {
+    return(array(x, dim=c(dim(x), 1)))
+  } else if (is.hexarray(x)) {
+    return(x)
+  } else {
+    stop('Conversion to hexarray failed.')
+  }
+}
+
+
+#' @export
 nlayer <- function(x) {
   dim(x)[3L]
 }
